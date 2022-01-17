f2cb9d3ed6cd35bc0b549168efeb96e32451323f
WindaWulandari9 <97888637+WindaWulandari9@users.noreply.github.com>
2022-01-17 19:12:55 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang  <strong><?=$_SESSION['username']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi Pendaftaran Siswa Baru TK/PAUD SYASTI</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/logo_paud.png"></img></p>
+                          <p class="label-title" align="center"><strong>TK/PAUD SYASTI</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Penerimaan Siswa Baru Tk/Paud Syasti.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data pendaftaran, pengelolaan data penerimaan, <br>
+                            dan juga pengelolaan laporan pendaftaran dan penerimaannya.<br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data pendaftaran dan data penerimaan.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead>
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
